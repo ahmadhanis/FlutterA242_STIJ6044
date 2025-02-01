43d0ae4f474c25839d14442fc88dfd48c03f33bb
Ahmad Hanis <36687333+ahmadhanis@users.noreply.github.com>
2025-02-01 11:05:56 +0800

    image container

--- mytukang/lib/newtukangscreen.dart
+++ mytukang/lib/newtukangscreen.dart
@@ -1,3 +1,4 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 
 class NewTukangScreen extends StatefulWidget {
@@ -27,6 +28,8 @@ class _NewTukangScreenState extends State<NewTukangScreen> {
     'Yan'
   ];
 
+  File? image;
+
   String selectedDistrict = 'Baling';
 
   @override
@@ -39,65 +42,91 @@ class _NewTukangScreenState extends State<NewTukangScreen> {
         body: Center(
           child: Padding(
             padding: const EdgeInsets.all(16.0),
-            child: Column(
-              children: [
-                TextField(
-                  controller: nameController,
-                  decoration: const InputDecoration(
-                    labelText: 'Name',
-                    border: OutlineInputBorder(),
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  Container(
+                    height: 200,
+                    decoration: BoxDecoration(
+                        border: Border.all(color: Colors.black),
+                        image: DecorationImage(
+                          image: image == null
+                              ? const AssetImage('assets/images/profile.png')
+                              : FileImage(image!) as ImageProvider<Object>,
+                          fit: BoxFit.contain,
+                        )),
                   ),
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                TextField(
-                  controller: emailController,
-                  decoration: const InputDecoration(
-                    labelText: 'Email',
-                    border: OutlineInputBorder(),
+                  const SizedBox(
+                    height: 10,
                   ),
-                  keyboardType: TextInputType.emailAddress,
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                TextField(
-                  controller: phoneController,
-                  decoration: const InputDecoration(
-                    labelText: 'Phone',
-                    border: OutlineInputBorder(),
+                  TextField(
+                    controller: nameController,
+                    decoration: const InputDecoration(
+                      labelText: 'Name',
+                      border: OutlineInputBorder(),
+                    ),
                   ),
-                  keyboardType: TextInputType.phone,
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                Container(
-                  height: 60,
-                  padding: const EdgeInsets.symmetric(horizontal: 15),
-                  decoration: BoxDecoration(
-                      border: Border.all(color: Colors.grey),
-                      borderRadius: BorderRadius.circular(5)),
-                  child: DropdownButton(
-                    value: selectedDistrict,
-                    underline: const SizedBox(),
-                    isExpanded: true,
-                    icon: const Icon(Icons.keyboard_arrow_down),
-                    items: districts.map((String items) {
-                      return DropdownMenuItem(
-                        value: items,
-                        child: Text(items),
-                      );
-                    }).toList(),
-                    onChanged: (String? newValue) {
-                      selectedDistrict = newValue!;
-                      print(selectedDistrict);
-                      setState(() {});
-                    },
+                  const SizedBox(
+                    height: 10,
                   ),
-                ),
-              ],
+                  TextField(
+                    controller: emailController,
+                    decoration: const InputDecoration(
+                      labelText: 'Email',
+                      border: OutlineInputBorder(),
+                    ),
+                    keyboardType: TextInputType.emailAddress,
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  TextField(
+                    controller: phoneController,
+                    decoration: const InputDecoration(
+                      labelText: 'Phone',
+                      border: OutlineInputBorder(),
+                    ),
+                    keyboardType: TextInputType.phone,
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Container(
+                    height: 60,
+                    padding: const EdgeInsets.symmetric(horizontal: 15),
+                    decoration: BoxDecoration(
+                        border: Border.all(color: Colors.grey),
+                        borderRadius: BorderRadius.circular(5)),
+                    child: DropdownButton(
+                      value: selectedDistrict,
+                      underline: const SizedBox(),
+                      isExpanded: true,
+                      icon: const Icon(Icons.keyboard_arrow_down),
+                      items: districts.map((String items) {
+                        return DropdownMenuItem(
+                          value: items,
+                          child: Text(items),
+                        );
+                      }).toList(),
+                      onChanged: (String? newValue) {
+                        selectedDistrict = newValue!;
+                        print(selectedDistrict);
+                        setState(() {});
+                      },
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  SizedBox(
+                    width: 400,
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      child: const Text('Submit'),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ));
